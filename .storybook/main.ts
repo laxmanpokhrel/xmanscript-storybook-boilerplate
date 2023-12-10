ca75b260b40d2bd2bfe37751bbe1c544fe1b8cf8
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2023-12-10 18:51:53 +0545

    fix: depreciation and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,28 +1,28 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-const { mergeConfig } = require("vite");
+import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 // use `mergeConfig` to recursively merge Vite options
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
   async viteFinal(config) {
     // Merge custom configuration into the default config
     return mergeConfig(config, {
       // Add storybook-specific dependencies to pre-optimization
       optimizeDeps: {
-        include: ["storybook-addon-designs"],
+        include: ['storybook-addon-designs'],
       },
     });
   },
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,7 @@
-import "tailwindcss/tailwind.css";
-import "@fontsource/material-icons";
+import 'tailwindcss/tailwind.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
